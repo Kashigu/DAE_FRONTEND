ad25359695e8f77b3fba4ec6d6ab18588d2a208a
Kashigu <orlandojslopes@gmail.com>
2024-11-26 18:01:37 +0000

    FrontEnd

---
+++ src/layout.vue
@@ -0,0 +1,25 @@
+<template>
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      </head>
+  
+      <body>
+        <Header />
+          <main>
+            <slot></slot> <!-- This will render the child content -->
+          </main>
+      </body>
+  </template>
+  
+  <script>
+  import Header from '@/components/Header.vue';
+  
+  export default {
+    name: 'Layout',
+    components: {
+      Header
+    },
+  };
+  </script>
+  
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    './index.html',  // If you're using an HTML file
+    './src/**/*.{vue,js,ts,jsx,tsx}', // If you're using Vue or JavaScript/TypeScript files in the src directory
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
